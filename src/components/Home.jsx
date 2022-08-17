faee2bb706a6498dc5abf7cce9187d1077403f22
Mykhailo.Koshmai <mykhailo.koshmai@dataart.com>
2022-08-17 21:57:34 +0300

    added i18n

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,19 @@
 import ButtonPush from "./ButtonPush";
+import { useTranslation } from "react-i18next";
+
+import { texts } from "../lang";
 
 const Home = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="home">
       <h1>Home</h1>
+      <h3>{t(texts.titleMain)}</h3>
       <hr />
-      <p>Push state and change url <ButtonPush /></p>
-      
+      <p>
+        Push state and change url <ButtonPush />
+      </p>
     </div>
   );
 };
